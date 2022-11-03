1b9fe69baa6c861b200584f1581287fad1c9f071
Imri Tull <111070159+xretraces@users.noreply.github.com>
2022-11-03 10:21:52 -0400

    Create Poem.java

---
+++ Poem.java
@@ -0,0 +1,43 @@
+// Imri Tull
+// Intermediate Programming 2150
+// November 1st 20222
+
+package Poem;
+public class Poem {
+
+    private String name;
+    private String poet;
+
+    // no arg constructor
+
+    public Poem() {
+        name = "unknown";
+        poet = "unknown";
+    }
+
+    // this will return the name
+
+    public String getName() {
+        return name;
+    }
+
+    // this will set the name
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPoet() {
+        return poet;
+    }
+        public void setPoet(String poet){
+            this.poet = poet;
+        }
+
+    public int setPoet() {
+
+        return 0;
+    }
+}
+
+
